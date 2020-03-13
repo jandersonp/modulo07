42598d14f3e980c8e3bd33b7d6cef459a68f735b
Janderson <jandersonpizziallyvilasboas@gmail.com>
2020-03-13 11:43:43 -0300

    chore: Criação do componente Header e seu estilo

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import { BrowserRouter } from 'react-router-dom';
 /** Rotas da nossa aplicação */
 import Routes from './routes';
 
+/** Importando o Componente Header  */
+import Header from './components/Header';
+
 /** Estilos Globais da Aplicação */
 import GlobalStyles from './styles/global';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <GlobalStyles />
       <Routes />
     </BrowserRouter>
