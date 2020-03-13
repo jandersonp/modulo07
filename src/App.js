b69410124d9d7afa5181c8d2aa8d3470367a4d8a
Janderson <jandersonpizziallyvilasboas@gmail.com>
2020-03-13 11:20:16 -0300

    chore: Criando nossos estilos globais

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import { BrowserRouter } from 'react-router-dom';
 /** Rotas da nossa aplicação */
 import Routes from './routes';
 
+/** Estilos Globais da Aplicação */
+import GlobalStyles from './styles/global';
+
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyles />
       <Routes />
     </BrowserRouter>
   );
