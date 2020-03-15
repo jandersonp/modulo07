eec91cd9f60682f7463ef259b07d1d8eed443251
Janderson <jandersonpizziallyvilasboas@gmail.com>
2020-03-15 16:26:35 -0300

    chore: Estilizando a página Home

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,36 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { Container, ProdutTable, Total } from './styles';
 
 export default function Cart() {
-  return <h1>Cart</h1>;
+  return (
+    <Container>
+      <ProdutTable>
+        <thead>
+          <tr>
+            <th />
+            <th>PRODUTO</th>
+            <th>QUANTIDADE</th>
+            <th>SUBTOTAL</th>
+            <th />
+          </tr>
+          <tbody>
+            <tr>
+              <td>
+                <img src="" alt="" srcSet="" />
+              </td>
+            </tr>
+          </tbody>
+        </thead>
+      </ProdutTable>
+
+      <footer>
+        <button type="button">Finalizar pedido</button>
+        <Total>
+          <span>TOTAL: </span>
+          <strong>$1999</strong>
+        </Total>
+      </footer>
+    </Container>
+  );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,67 @@
 import React from 'react';
+import { MdAddShoppingCart } from 'react-icons/md';
 
-// import { Container } from './styles';
+import { ProductList } from './styles';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-harden-stepback-masculino/82/NQQ-0224-782/NQQ-0224-782_detalhe1.jpg?ts=1579783590?resize=280:280"
+          alt="Tênis "
+        />
+        <strong>Tênis muito legal</strong>
+        <span>$329,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-harden-stepback-masculino/82/NQQ-0224-782/NQQ-0224-782_detalhe1.jpg?ts=1579783590?resize=280:280"
+          alt="Tênis "
+        />
+        <strong>Tênis muito legal</strong>
+        <span>$329,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-harden-stepback-masculino/82/NQQ-0224-782/NQQ-0224-782_detalhe1.jpg?ts=1579783590?resize=280:280"
+          alt="Tênis "
+        />
+        <strong>Tênis muito legal</strong>
+        <span>$329,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/tenis-adidas-harden-stepback-masculino/82/NQQ-0224-782/NQQ-0224-782_detalhe1.jpg?ts=1579783590?resize=280:280"
+          alt="Tênis "
+        />
+        <strong>Tênis muito legal</strong>
+        <span>$329,90</span>
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#FFF" /> 3
+          </div>
+          <span>ADICIONAR AO CARRINHO</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
